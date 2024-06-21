ac9acb4ec46b022f8e917df6dd290890ac874fff
Trisha-tech <1234trishasahu@gmail.com>
2024-06-21 21:25:40 +0530

    Added Server

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors({
+  origin: ['https://localhost:8080',],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+
+
+
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
+module.exports = app;
