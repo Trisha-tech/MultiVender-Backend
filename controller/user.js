ec99042eb5ed13214db40e022bf3977b1c0d7c21
Trisha-tech <1234trishasahu@gmail.com>
2024-06-22 19:18:00 +0530

    User Signup Route Added

---
+++ controller/user.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const User = require("../model/user");
+const router = express.Router();
+const cloudinary = require("cloudinary");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
+const sendToken = require("../utils/jwtToken");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
+
+// create user
+router.post("/create-user", async (req, res, next) => {
+    try {
+      const { name, email, password, avatar } = req.body;
+      const userEmail = await User.findOne({ email });
+  
+      if (userEmail) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
+  
+      const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+        folder: "avatars",
+      });
+  
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        },
+      };
+  
+      const activationToken = createActivationToken(user);
+  
+      const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+  
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Activate your account",
+          message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+        });
+        res.status(201).json({
+          success: true,
+          message: `please check your email:- ${user.email} to activate your account!`,
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  });
+  
+  // create activation token
+  const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+      expiresIn: "5m",
+    });
+  };
+  
+  // activate user
+  router.post(
+    "/activation",
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        const { activation_token } = req.body;
+  
+        const newUser = jwt.verify(
+          activation_token,
+          process.env.ACTIVATION_SECRET
+        );
+  
+        if (!newUser) {
+          return next(new ErrorHandler("Invalid token", 400));
+        }
+        const { name, email, password, avatar } = newUser;
+  
+        let user = await User.findOne({ email });
+  
+        if (user) {
+          return next(new ErrorHandler("User already exists", 400));
+        }
+        user = await User.create({
+          name,
+          email,
+          avatar,
+          password,
+        });
+  
+        sendToken(user, 201, res);
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
