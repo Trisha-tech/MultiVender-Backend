7951e5669f76691ee01520b829ec36cac209b79d
Trisha-tech <1234trishasahu@gmail.com>
2024-06-22 19:21:33 +0530

    User Logout Route Added

--- controller/user.js
+++ controller/user.js
@@ -132,3 +132,45 @@ router.post(
   );
   
   
+// load user
+router.get(
+    "/getuser",
+    isAuthenticated,
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        const user = await User.findById(req.user.id);
+  
+        if (!user) {
+          return next(new ErrorHandler("User doesn't exists", 400));
+        }
+  
+        res.status(200).json({
+          success: true,
+          user,
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
+  
+  // log out user
+  router.get(
+    "/logout",
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        res.cookie("token", null, {
+          expires: new Date(Date.now()),
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+        });
+        res.status(201).json({
+          success: true,
+          message: "Log out successful!",
+        });
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
